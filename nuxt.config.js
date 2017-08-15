20110fc518f111050437338e5d720182f68a624a
Guangyu Lin <linguangyu@bbcloud.com>
2017-08-15 19:35:01 +0800

    try the router

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,11 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#3B8070' },
+  loading: '~/components/loading.vue',
+  // loading: { 
+  //   color: '#3B8070',
+  //   height: '5px'
+  // },
   /*
   ** Build configuration
   */
---
+++ pages/_id/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>测试</div>
+</template>
+
+<script>
+export default {
+  validate ({ params, query, store }) {
+    console.log(params)
+    return true // if the params are valid
+  }
+}
+</script>
